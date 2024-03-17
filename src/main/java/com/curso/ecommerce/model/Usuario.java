0424275f5c76708c106837de0c4630fe3a844dd6
NiccolasBon <agustindenis03@gmail.com>
2024-03-17 10:34:30 -0300

    crear las clases model, mapeo clases a tablas con jpa, integrado la parte de diseño y vistas

--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,7 +1,19 @@
 package com.curso.ecommerce.model;
 
-public class Usuario {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -11,11 +23,17 @@ public class Usuario {
 	private String tipo;
 	private String password;
 
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> listaProductos;
+
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> listaOrdenes;
+
 	public Usuario() {
 	}
 
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
-			String tipo, String password) {
+			String tipo, String password, List<Producto> listaProductos, List<Orden> listaOrdenes) {
 		this.id = id;
 		this.nombre = nombre;
 		this.username = username;
@@ -24,6 +42,8 @@ public class Usuario {
 		this.telefono = telefono;
 		this.tipo = tipo;
 		this.password = password;
+		this.listaProductos = listaProductos;
+		this.listaOrdenes = listaOrdenes;
 	}
 
 	public Integer getId() {
@@ -90,11 +110,27 @@ public class Usuario {
 		this.password = password;
 	}
 
+	public List<Producto> getListaProductos() {
+		return listaProductos;
+	}
+
+	public void setListaProductos(List<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+
+	public List<Orden> getListaOrdenes() {
+		return listaOrdenes;
+	}
+
+	public void setListaOrdenes(List<Orden> listaOrdenes) {
+		this.listaOrdenes = listaOrdenes;
+	}
+
 	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", email=" + email
 				+ ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password
-				+ "]";
+				+ ", listaProductos=" + listaProductos + ", listaOrdenes=" + listaOrdenes + "]";
 	}
 
 }
