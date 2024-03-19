6ac9bcc75b25b4ba4a6d26c9edfe5acb23e3003c
NiccolasBon <agustindenis03@gmail.com>
2024-03-18 21:08:52 -0300

    implementar las funcionalidades de cargar imnagen y mostrar imagen para el admin

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -55,14 +55,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		} else {
-			if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagen
-				Producto p = new Producto();
-				p = productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			} else {
-				String nombreImagen = upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+
 		}
 
 		productoService.save(producto);
@@ -84,13 +77,41 @@ public class ProductoController {
 	}
 
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+
+		if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagen
+
+			producto.setImagen(p.getImagen());
+		} else { // aqui es cuando se edita tmb la imagen
+
+			// eliminar cuando no sea la img por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+
+		producto.setUsuario(p.getUsuario());
+
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+
+		// eliminar cuando no sea la img por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
