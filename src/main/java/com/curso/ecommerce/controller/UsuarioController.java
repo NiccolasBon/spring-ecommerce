b6750d9977c0eeb2242a594e1a42fc96276907fe
NiccolasBon <agustindenis03@gmail.com>
2024-03-21 22:48:05 -0300

    funcionalidad mostrar la vista compras para el user

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,12 @@ public class UsuarioController {
 		}
 
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compras")
+	public String obtenerCompras(HttpSession miSession, Model model) {
+		model.addAttribute("miSession", miSession.getAttribute("idusuario"));
+		return "usuario/compras";
 	}
 
 }
